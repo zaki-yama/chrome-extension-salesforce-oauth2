b83fe9b17b12573b524ecab9b93cd5cc8acc5b47
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-07-02 07:01:26 +0900

    wip: Add Login button

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,25 +1,32 @@
 import * as React from 'react';
 import './Popup.scss';
 
-interface AppProps {}
+interface AppProps {
+    onClickLogin: () => void,
+}
 
 interface AppState {}
 
 export default class Popup extends React.Component<AppProps, AppState> {
-    constructor(props: AppProps, state: AppState) {
-        super(props, state);
-    }
+  constructor(props: AppProps, state: AppState) {
+    super(props, state);
+  }
 
-    componentDidMount() {
-        // Example of how to send a message to eventPage.ts.
-        chrome.runtime.sendMessage({ popupMounted: true });
-    }
+  componentDidMount() {
+    // Example of how to send a message to eventPage.ts.
+    chrome.runtime.sendMessage({ popupMounted: true });
+  }
 
-    render() {
-        return (
-            <div className="popupContainer">
-                Hello, world!
-            </div>
+  onClickLogin() {
+      console.log('clicked');
+        console.log(chrome.identity);
+  }
+
+  render() {
+    return (
+        <div className="popupContainer">
+            <button onClick={this.onClickLogin}>Login</button>
+        </div>
         )
-    }
+  }
 }
