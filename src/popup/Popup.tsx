fbccfaae3f1f3ba339a71e46db2ee78f29d8928a
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-07-03 21:42:47 +0900

    Get access token

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -6,6 +6,12 @@ interface AppProps {
 
 interface AppState { }
 
+const OAUTH_ENDPOINT = 'https://login.salesforce.com/services/oauth2/authorize';
+const CLIENT_ID = '3MVG9I1kFE5Iul2BYKzI252s0YFYfPhssmER1TlqMPEThx8Xu0I6lvvH1EI6LNlWRFDRVT9bbVQCIKUgP8bTI';
+
+const APP_ID = 'jedlgfncdibhobnepodlejklecicphgd';
+const REDIRECT_URI = `https://${APP_ID}.chromiumapp.org/provider_cb&response_type=token`;
+
 export default class Popup extends React.Component<AppProps, AppState> {
   constructor(props: AppProps, state: AppState) {
     super(props, state);
@@ -19,6 +25,29 @@ export default class Popup extends React.Component<AppProps, AppState> {
   onClickLogin() {
     console.log('clicked');
     console.log(chrome);
+    chrome.identity.launchWebAuthFlow({
+      url: `${OAUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`,
+      interactive: true
+    }, (authorizeResponse) => {
+      console.log(authorizeResponse);
+
+      const parseQueryString = (url) => {
+        const res = {};
+        const hash = new URL(url).hash;
+        // Remove #
+        const pairs = hash.slice(1).split('&');
+        for (let i = 0; pairs[i]; i++) {
+          var keyValue = pairs[i].split('=');
+          res[keyValue[0]] = keyValue[1];
+        }
+        return res;
+      }
+
+      const params = parseQueryString(authorizeResponse);
+
+      const token = params.access_token;
+      console.log('token', params, token);
+    });
   }
 
   render() {
